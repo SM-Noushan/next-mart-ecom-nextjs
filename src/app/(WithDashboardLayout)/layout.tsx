0f9a15c2d114d9026c94c78fd4ca7df8cae33d04
SMNoushan <noushan.md@gmail.com>
2025-05-13 21:43:26 +0600

    add: dashboard layout + new shadcn comp

--- src/app/(WithDashboardLayout)/layout.tsx
+++ src/app/(WithDashboardLayout)/layout.tsx
@@ -1,6 +1,24 @@
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/dashboard/sidebar/app-sidebar";
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-    return children;
-  };
-  
-  export default DashboardLayout;
-  
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+          </div>
+        </header>
+        <div className="min-h-[calc(100dvh-100px)] p-4 pt0">{children}</div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+};
+
+export default DashboardLayout;
---
+++ src/components/dashboard/sidebar/app-sidebar.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import Link from "next/link";
+import * as React from "react";
+import {
+  Sidebar,
+  SidebarMenu,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarContent,
+  SidebarMenuItem,
+  SidebarMenuButton,
+} from "@/components/ui/sidebar";
+import { NavUser } from "./nav-user";
+import Logo from "@/app/assets/svgs/Logo";
+import { Bot, Settings2, SquareTerminal } from "lucide-react";
+import { NavMain } from "@/components/dashboard/sidebar/nav-main";
+
+const data = {
+  navMain: [
+    {
+      title: "Dashboard",
+      url: "/user/dashboard",
+      icon: SquareTerminal,
+      isActive: true,
+    },
+    {
+      title: "Shop",
+      url: "#",
+      icon: Bot,
+      items: [
+        {
+          title: "Manage Products",
+          url: "/user/shop/all-products",
+        },
+        {
+          title: "Manage Categories",
+          url: "/user/shop/category",
+        },
+        {
+          title: "Manage Brands",
+          url: "/user/shop/brand",
+        },
+      ],
+    },
+    {
+      title: "Settings",
+      url: "#",
+      icon: Settings2,
+      items: [
+        {
+          title: "Profile",
+          url: "/profile",
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar variant="inset" {...props}>
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton size="lg" asChild>
+              <Link href="/">
+                <div className="flex items-center justify-center">
+                  <Logo />
+                </div>
+                <h2 className="font-bold text-xl">NextMart</h2>
+              </Link>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={data.navMain} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser />
+      </SidebarFooter>
+    </Sidebar>
+  );
+}
